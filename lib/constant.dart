3017b8e193651e949ea79cd159d306b89965f937
Junaid <muhammadjunaid9394@gmail.com>
2024-01-24 19:58:50 +0500

    adding constant

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff1098c2);
 const kTextFieldColor = Color(0xffefefef);
 const kTextFieldTextColor = Color(0xffafafaf);
+
+const kTextFieldTextColor1 = Color.fromARGB(255, 112, 39, 186);
